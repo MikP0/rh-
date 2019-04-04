900a2070dfb2119000612a0ecc6b56e360ef7181
Fabian Frontczak <fabian_frontczak@wp.pl>
2019-04-05 00:18:37 +0200

    Left only attributes in AudioComponent

--- rh-/rh-/AudioComponent.h
+++ rh-/rh-/AudioComponent.h
@@ -5,26 +5,15 @@
 class AudioComponent : public Component
 {
 public:
+	std::string _path;
+	bool _mute, _playOnAwake, _loop;
+	float _volume;
+
 	AudioComponent();
 	AudioComponent(std::string path);
 	~AudioComponent();
 
-	std::string GetPath();
-	void SetPath(std::string path);
-	bool GetMute();
-	void SetMute(bool muteState);
-	bool GetPlayOnAwake();
-	void SetPlayOnAwake(bool playOnAwakeState);
-	void SetLoop(bool loopState);
-	bool GetLoop();
-	float GetVolume();
-	void SetVolume(float volume);
-
 	virtual ComponentType GetType(void) override;
-
-private:
-	std::string _path;
-	bool _mute, _playOnAwake, _loop;
-	float _volume;
+	
 };
 
