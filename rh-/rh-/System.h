f987bdb2cad85973300d8c6d7dcee3bd0089e6dc
Fabian Frontczak <fabian_frontczak@wp.pl>
2019-04-04 15:05:44 +0200

    Fixed

--- rh-/rh-/System.h
+++ rh-/rh-/System.h
@@ -10,9 +10,9 @@ class System
 public:
 	System();
 	virtual ~System();
-	//virtual std::vector<std::shared_ptr<System>> GetComponents() = 0;
+	//virtual std::vector<std::shared_ptr<Component>> GetComponents() = 0;
 
 protected:
 	virtual void Iterate() = 0;
-	//std::vector<std::shared_ptr<System>> _components;
+	//std::vector<std::shared_ptr<Component>> _components;
 };
