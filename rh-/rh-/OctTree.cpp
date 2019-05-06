53d3f7f48c45b8c9c048d672ac64efea9b4a0743
Fabian Frontczak <fabian_frontczak@wp.pl>
2019-05-06 18:30:40 +0200

    Częściowo wypełniono metodę BuildTree

--- rh-/rh-/OctTree.cpp
+++ rh-/rh-/OctTree.cpp
@@ -13,7 +13,7 @@ bool OctTree::_treeBuilt = false;
 
 //The bounding region for the oct tree. 
 //The list of objects contained within the bounding region 
-OctTree::OctTree(BoundingBox region, list<shared_ptr<PhysicsComponent>> objList)
+OctTree::OctTree(MyBoundingBox region, list<shared_ptr<PhysicsComponent>> objList)
 {
 	_region = region;
 	_objects = objList;
@@ -26,7 +26,7 @@ OctTree::OctTree()
 		_childNode[i] = make_shared<OctTree>();
 
 	_objects = list<shared_ptr<PhysicsComponent>>();
-	_region = BoundingBox(Vector3::Zero, Vector3::Zero);
+	_region = MyBoundingBox(Vector3::Zero, Vector3::Zero);
 	_curLife = -1;
 }
 
@@ -36,7 +36,7 @@ OctTree::OctTree()
 
 ///The suggested dimensions for the bounding region. 
 ///Note: if items are outside this region, the region will be automatically resized. 
-OctTree::OctTree(BoundingBox region)
+OctTree::OctTree(MyBoundingBox region)
 {
 	_region = region;
 	_objects = list<shared_ptr<PhysicsComponent>>();
@@ -58,7 +58,7 @@ void OctTree::UpdateTree() //complete & tested
 			_objects.push_back(element);
 		}
 			
-		//BuildTree();
+		BuildTree();
 	}
 	else
 	{
@@ -71,4 +71,55 @@ void OctTree::UpdateTree() //complete & tested
 	}
 
 	_treeReady = true;
+}
+
+/// 
+/// Naively builds an oct tree from scratch.
+/// 
+void OctTree::BuildTree() //complete & tested
+{
+	//terminate the recursion if we're a leaf node
+	if (_objects.size() <= 1)
+		return;
+
+	Vector3 dimensions = _region.Max - _region.Min;
+
+	//Check to see if the dimensions of the box are greater than the minimum dimensions
+	if (dimensions.x <= MIN_SIZE && dimensions.y <= MIN_SIZE && dimensions.z <= MIN_SIZE)
+	{
+		return;
+	}
+
+	Vector3 center = _region.Box.Center;
+
+	//Create subdivided regions for each octant
+	shared_ptr<MyBoundingBox> octant[8];
+
+	octant[0] = make_shared<MyBoundingBox>(_region.Min, center);
+	octant[1] = make_shared<MyBoundingBox>(Vector3(center.x, _region.Min.y, _region.Min.z), Vector3(_region.Max.x, center.y, center.z));
+	octant[2] = make_shared<MyBoundingBox>(Vector3(center.x, _region.Min.y, center.z), Vector3(_region.Max.x, center.y, _region.Max.z));
+	octant[3] = make_shared<MyBoundingBox>(Vector3(_region.Min.x, _region.Min.y, center.z), Vector3(center.x, center.y, _region.Max.z));
+	octant[4] = make_shared<MyBoundingBox>(Vector3(_region.Min.x, center.y, _region.Min.z), new Vector3(center.x, _region.Max.y, center.z));
+	octant[5] = make_shared<MyBoundingBox>(Vector3(center.x, center.y, _region.Min.z), Vector3(_region.Max.x, _region.Max.y, center.z));
+	octant[6] = make_shared<MyBoundingBox>(center, _region.Max);
+	octant[7] = make_shared<MyBoundingBox>(Vector3(_region.Min.x, center.y, center.z), new Vector3(center.x, _region.Max.y, _region.Max.z));
+
+}
+
+shared_ptr<OctTree> OctTree::CreateNode(MyBoundingBox region, list<shared_ptr<PhysicsComponent>> objList) //complete & tested
+{
+	if (objList.size() == 0)
+		return nullptr;
+	shared_ptr<OctTree> ret = make_shared<OctTree>(region, objList);
+	ret->_parent = shared_from_this();
+	return ret;
+}
+
+shared_ptr<OctTree> OctTree::CreateNode(MyBoundingBox region, shared_ptr<PhysicsComponent> Item)
+{
+	list<shared_ptr<PhysicsComponent>> objList(1); //sacrifice potential CPU time for a smaller memory footprint
+	objList.push_back(Item);
+	shared_ptr<OctTree> ret = make_shared<OctTree>(region, objList);
+	ret->_parent = shared_from_this();
+	return ret;
 }
