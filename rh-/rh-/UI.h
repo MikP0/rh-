4e44fc3de4cdd219e70456cedc9d0cab5d89a2f0
Fabian Frontczak <fabian_frontczak@wp.pl>
2019-06-13 00:49:27 +0200

    Uporządkowano klasę UI

--- rh-/rh-/UI.h
+++ rh-/rh-/UI.h
@@ -1,8 +1,21 @@
 #pragma once
 
+#include <map>
+
 using namespace std;
 using namespace DirectX;
 using namespace DirectX::SimpleMath;
+
+struct UiElement
+{
+	string name;
+	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> texture;
+	Vector2 position;
+	Vector2 scale;
+
+	UiElement() : name("None"), position(Vector2(0.0f, 0.0f)), scale(Vector2(1.0f, 1.0f)) {};
+	~UiElement() {};
+};
 
 class UI
 {
@@ -15,49 +28,8 @@ public:
 	void Draw(bool vampireMode, bool menuIsOn);
 	void Reset();
 
-	//UI Sprites
-	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> healthBarTex;
-	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> healthBarHeroTex;
-	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> healthBarHealthTex;
-	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> redBorderTex;
-	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> normalAttackTex;
-	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> powerAttackTex;
-	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> spinAttackTex;
-	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> biteAttackTex;
-	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> swapAttackTex;
-	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> aoeAttackTex;
-	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> dashTex;
-	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> heroCircleTex;
-	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> ripTex;
 
-	Vector2 healthBarPos;
-	Vector2 healthBarHeroPos;
-	Vector2 healthBarHealthPos;
-	Vector2 healthBarHealthScale;
-	Vector2 redBorderPos;
-	Vector2 redBorderScale;
-	Vector2 normalAttackPos;
-	Vector2 normalAttackScale;
-	Vector2 powerAttackPos;
-	Vector2 powerAttackScale;
-	Vector2 spinAttackPos;
-	Vector2 spinAttackScale;
-	Vector2 biteAttackPos;
-	Vector2 biteAttackScale;
-	Vector2 swapAttackPos;
-	Vector2 swapAttackScale;
-	Vector2 aoeAttackPos;
-	Vector2 aoeAttackScale;
-	Vector2 dashPos;
-	Vector2 dashScale;
-	Vector2 heroCircleAttackPos;
-	Vector2 heroCircleAttackScale;
-	Vector2 ripAttackPos;
-	Vector2 ripAttackScale;
 	Vector2 skillSetPosition;
-
-	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> fpsBarTex;
-	Vector2 fpsBarPos;
 
 	shared_ptr<DirectX::SpriteBatch> uiSpriteBatch;
 	shared_ptr<DirectX::SpriteBatch> uiSpriteBatchBorder;
@@ -66,14 +38,12 @@ public:
 	Vector2 fpsFontPos;
 	wstring fpsFontText;
 
-	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> menuTex;
-	Vector2 menuPos;
-
 
 private:
 	ID3D11Device1 *_device;
 	ID3D11DeviceContext1 *_context;
 	float _playerHealthOrigin;
 	shared_ptr<float> _playerHealth;
+	map<string, UiElement> _elements;
 };
 
